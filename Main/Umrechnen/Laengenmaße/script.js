a415292f734b8ec48cc20b7f23f287697730d253
Julian Clement <juli.clement@icloud.com>
2025-06-26 15:56:22 +0200

    Html Meters Done and begin to type script

---
+++ Main/Umrechnen/Laengenmaße/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    // Lots of variables xD
+    const USPEH = document.getElementById("USPEH");
+    const ZielEH = document.getElementById("ZielEH");
+    let MeterSubmitBTN = document.getElementById("MeterSubmitBTN");
+    const MeterInputNUM = document.getElementById("MeterInputNUM");
+    const MeterOutputNUM = document.getElementById("MeterOutputNUM");
+
+    document.getElementById("MeterSubmitBTN").addEventListener("click", function() {
+        
+    });
+});
