eed49b185f5d4f3c31330a5f97b2af490597ab14
Pieti30tv <juli.clement@icloud.com>
2025-07-11 11:16:24 +0200

    KFB: 1.0

--- Main/Kreisflaeche/script.js
+++ Main/Kreisflaeche/script.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("Submit").addEventListener("click", function() {
-
         let Durchmesser = document.getElementById("D-Input").value;
-        
         try {
-
             if (isNaN(Durchmesser) || Durchmesser <= 0) {
                 document.getElementById("Error-OUT").innerHTML = "Bitte geben Sie eine gültige Zahl über 0 ein!";
                 return;
             }
-
         }
         catch(e) {
             console.error("Error: " + e.message);
@@ -36,6 +32,22 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 });
+
+function calcSurface(Durchmesser) {
+    try {
+        if (isNaN(Durchmesser) || Durchmesser <= 0) {
+            throw new Error("Bitte geben Sie eine gültige Zahl über 0 ein!");
+        }
+    } catch (e) {
+        console.error("Error: " + e.message);
+        document.getElementById("Error-OUT").innerHTML = e.message;
+        return;
+    }
+
+    let Radius = Durchmesser / 2;
+    let Rad2 = Radius * Radius;
+    let Fläche = Math.PI * Rad2;
+}
 
 
 
