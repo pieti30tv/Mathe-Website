fa7b603be0f19d7cee88bf439cee2871130ae6b3
Pieti30tv <juli.clement@icloud.com>
2025-07-11 11:38:35 +0200

    KFB: 2.0

--- Main/Kreisflaeche/script.js
+++ Main/Kreisflaeche/script.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    document.getElementById("D-Input").addEventListener("input", function(event) {
+        // Prevent non-numeric input
+        /*
+        if (!/[0-9]/.test(event.key) && event.key !== "Backspace" && event.key !== "Enter") {
+            // Log key press for debugging
+            console.log("Invalid key pressed in D-Input", event);
+            event.preventDefault();
+        }
+        */
+        let Durchmesser = document.getElementById("D-Input").value
+        let Fläche = calcSurface(Durchmesser);
+        
+        updateOutput(Fläche);
+    });
+
+
     document.getElementById("Submit").addEventListener("click", function() {
         let Durchmesser = document.getElementById("D-Input").value;
         try {
@@ -14,20 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         console.log(Durchmesser);
+        let Fläche = calcSurface(Durchmesser);
 
-        Radius = Durchmesser / 2;
-        console.log(Radius);
-
-        Rad2 = Radius * Radius;
-        console.log(Rad2);
-
-        console.log("Berechnete Fläche: ");
-        let Fläche = Math.PI * Rad2
-        console.log(Fläche);
-
-        document.getElementById("D-Output").value = Fläche;
-
-        document.getElementById("Error-OUT").innerHTML = "";
+        updateOutput(Fläche);
 
     });
 
@@ -40,13 +45,24 @@ function calcSurface(Durchmesser) {
         }
     } catch (e) {
         console.error("Error: " + e.message);
-        document.getElementById("Error-OUT").innerHTML = e.message;
-        return;
+        return -1;
     }
 
     let Radius = Durchmesser / 2;
     let Rad2 = Radius * Radius;
     let Fläche = Math.PI * Rad2;
+
+    return Fläche;
+}
+
+function updateOutput(Fläche) {
+    if (Fläche == -1) {
+        document.getElementById("Error-OUT").innerHTML = "Bitte geben Sie eine gültige Zahl über 0 ein!";
+        document.getElementById("D-Output").value = "";
+        return;
+    }
+    document.getElementById("D-Output").value = Fläche;
+    document.getElementById("Error-OUT").innerHTML = "";
 }
 
 
